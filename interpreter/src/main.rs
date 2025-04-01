f194d699ec7c35917927ef28b56e260d776ad84f
A K <alex.kesling@gmail.com>
2025-03-31 22:02:43 -0400

    fix: ppm data is column-major and y should go 1->-1

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -15,11 +15,13 @@ struct Args {
     output: path::PathBuf,
 }
 
+type ValueType = f64;
+
 #[derive(Clone, Debug)]
 enum Instruction {
     VarX,
     VarY,
-    Const(f64),
+    Const(ValueType),
     Add(usize, usize),
     Sub(usize, usize),
     Mul(usize, usize),
@@ -44,7 +46,7 @@ impl Instruction {
         Ok(match (op, args) {
             ("var-x", None) => VarX,
             ("var-y", None) => VarY,
-            ("const", Some(args)) => Const(args.trim().parse::<f64>()?),
+            ("const", Some(args)) => Const(args.trim().parse::<ValueType>()?),
             ("add", Some(rest)) => {
                 let mut tokens = rest.split(" ");
                 let addr1 =
@@ -121,8 +123,8 @@ fn parse(input: &str) -> anyhow::Result<Vec<Instruction>> {
         .collect::<anyhow::Result<Vec<Instruction>>>()
 }
 
-fn interpret_point(program: &[Instruction], x: f64, y: f64) -> bool {
-    let mut context: Vec<f64> = vec![0.0; program.len()];
+fn interpret_point(program: &[Instruction], x: ValueType, y: ValueType) -> bool {
+    let mut context: Vec<ValueType> = vec![0.0; program.len()];
     for (i, inst) in program.iter().enumerate() {
         context[i] = match *inst {
             Instruction::VarX => x,
@@ -131,8 +133,8 @@ fn interpret_point(program: &[Instruction], x: f64, y: f64) -> bool {
             Instruction::Add(addr1, addr2) => context[addr1] + context[addr2],
             Instruction::Sub(addr1, addr2) => context[addr1] - context[addr2],
             Instruction::Mul(addr1, addr2) => context[addr1] * context[addr2],
-            Instruction::Max(addr1, addr2) => f64::max(context[addr1], context[addr2]),
-            Instruction::Min(addr1, addr2) => f64::min(context[addr1], context[addr2]),
+            Instruction::Max(addr1, addr2) => ValueType::max(context[addr1], context[addr2]),
+            Instruction::Min(addr1, addr2) => ValueType::min(context[addr1], context[addr2]),
             Instruction::Neg(addr) => -context[addr],
             Instruction::Square(addr) => context[addr].powf(2.0),
             Instruction::Sqrt(addr) => context[addr].powf(0.5),
@@ -142,26 +144,28 @@ fn interpret_point(program: &[Instruction], x: f64, y: f64) -> bool {
     *context.last().unwrap() < 0.0
 }
 
-fn index_to_point(i: usize, span: f64) -> f64 {
-    ((i as f64) / span) * 2.0 - 1.0
+fn index_to_point(i: usize, span: ValueType) -> ValueType {
+    ((i as ValueType) / span) * 2.0 - 1.0
 }
 
 fn interpret_image(program: &[Instruction], width: usize, height: usize) -> Vec<Vec<bool>> {
-    let mut image = vec![vec![false; height]; width];
-    let span_x = width as f64;
-    let span_y = height as f64;
-    let x_points: Vec<(usize, f64)> = (0..width).map(|x| (x, index_to_point(x, span_x))).collect();
-    let y_points: Vec<(usize, f64)> = if width == height {
-        x_points.clone()
-    } else {
-        (0..height)
-            .map(|y| (y, index_to_point(y, span_y)))
-            .collect()
-    };
+    let mut image = vec![vec![false; width]; height];
+    let span_x = width as ValueType;
+    let span_y = height as ValueType;
 
-    for (x_i, x) in x_points.into_iter() {
-        for (y_i, y) in y_points.iter() {
-            image[x_i][*y_i] = interpret_point(program, x, *y);
+    // NOTE: Overall iteration goes from (-1, 1) in the "upper left"
+    // to (1, -1) in the "lower right"
+    let y_points: Vec<(usize, ValueType)> = (0..height)
+        .map(|i| (i, index_to_point(height - i - 1, span_y)))
+        .collect();
+    let x_points: Vec<(usize, ValueType)> =
+        (0..width).map(|i| (i, index_to_point(i, span_x))).collect();
+
+    // From 1 to -1
+    for (y_i, y) in y_points.into_iter() {
+        // From -1 to 1
+        for (x_i, x) in x_points.iter() {
+            image[y_i][*x_i] = interpret_point(program, *x, y);
         }
     }
     image
@@ -173,7 +177,8 @@ async fn render(pixels: Vec<Vec<bool>>, path: &path::Path) -> anyhow::Result<()>
     let mut file = tokio::fs::File::create(path).await?;
     file.write_all(&format!("P5\n{} {}\n255\n", pixels.len(), pixels[0].len()).into_bytes())
         .await?;
-    for line in pixels {
+    // NOTE: PPM pixel data is column-major.
+    for line in pixels.into_iter() {
         file.write_all(
             &line
                 .into_iter()
